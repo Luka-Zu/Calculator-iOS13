47768fa9d2c293ac3232715a8bc383b2fc04b13b
LukaZukhbaia <95714793+LukaZukhbaia@users.noreply.github.com>
2022-08-05 20:48:58 +0400

    Update ViewController.swift
    finished version

--- Calculator Layout iOS13/ViewController.swift
+++ Calculator Layout iOS13/ViewController.swift
@@ -1,57 +1,107 @@
-//
-//  ViewController.swift
-//  Calculator Layout iOS13
-//
-//  Created by Angela Yu on 01/07/2019.
-//  Copyright © 2019 The App Brewery. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet weak var valueDisplay: UILabel!
     
+    
+    
     @IBAction func dot(_ sender: Any) {
+        
         valueDisplay.text=(valueDisplay.text ?? "0") + "."
     }
     
     @IBAction func zero(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "0"
+        if (valueDisplay.text ?? "0") != "0"{
+            valueDisplay.text=(valueDisplay.text ?? "0") + "0"
+        }
+        else{
+            valueDisplay.text="0"
+        }
+        
     }
     
     @IBAction func one(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "1"
+        if (valueDisplay.text ?? "0") != "0"{
+        
+            valueDisplay.text=(valueDisplay.text ?? "0") + "1"
+        }
+        else{
+            valueDisplay.text="1"
+        }
     }
     @IBAction func two(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "2"
+        if (valueDisplay.text ?? "0") != "0"{
+        
+            valueDisplay.text=(valueDisplay.text ?? "0") + "2"
+        }
+        else{
+            valueDisplay.text="2"
+        }
     }
     @IBAction func three(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "3"
+        if (valueDisplay.text ?? "0") != "0"{
+        
+            valueDisplay.text=(valueDisplay.text ?? "0") + "3"
+        }
+        else{
+            valueDisplay.text="3"
+        }
     }
     
     @IBAction func four(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "4"
+        if (valueDisplay.text ?? "0") != "0"{
+        
+            valueDisplay.text=(valueDisplay.text ?? "0") + "4"
+        }
+        else{
+            valueDisplay.text="4"
+        }
     }
     
     @IBAction func five(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "5"
+        if (valueDisplay.text ?? "0") != "0"{
+            valueDisplay.text=(valueDisplay.text ?? "0") + "5"
+        }
+        else{
+            valueDisplay.text="5"
+        }
     }
     
     @IBAction func six(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "6"
+        if (valueDisplay.text ?? "0") != "0"{
+            valueDisplay.text=(valueDisplay.text ?? "0") + "6"
+        }
+        else{
+            valueDisplay.text="6"
+        }
     }
     
     @IBAction func seven(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "7"
+        if (valueDisplay.text ?? "0") != "0"{
+            valueDisplay.text=(valueDisplay.text ?? "0") + "7"
+        }
+        else{
+            valueDisplay.text="7"
+        }
     }
     
     @IBAction func nine(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "9"
+        if (valueDisplay.text ?? "0") != "0"{
+            valueDisplay.text=(valueDisplay.text ?? "0") + "9"
+        }
+        else{
+            valueDisplay.text="9"
+        }
     }
     
     
     @IBAction func eight(_ sender: Any) {
-        valueDisplay.text=(valueDisplay.text ?? "0") + "8"
+        if (valueDisplay.text ?? "0") != "0"{
+            valueDisplay.text=(valueDisplay.text ?? "0") + "8"
+        }
+        else{
+            valueDisplay.text="8"
+        }
     }
     
     @IBAction func makeZero(_ sender: Any) {
@@ -68,13 +118,32 @@ class ViewController: UIViewController {
     @IBAction func minus(_ sender: Any) {
         valueDisplay.text=(valueDisplay.text ?? "0") + "-"
     }
-    
+    func formatResult(result: Double) -> String
+        {
+            if(result.truncatingRemainder(dividingBy: 1) == 0)
+            {
+                return String(format: "%.0f", result)
+            }
+            else
+            {
+                return String(format: "%.2f", result)
+            }
+    }
+    @IBAction func calculate(_ sender: Any) {
+        let checkedWorkingsForPercent = (valueDisplay.text ?? "0").replacingOccurrences(of: "%", with: "*0.01")
+        let expression = NSExpression(format: checkedWorkingsForPercent)
+        let result = expression.expressionValue(with: nil, context: nil) as! Double
+        let resultString = formatResult(result: result)
+        valueDisplay.text  = resultString
+        
+        
+    }
     @IBAction func plus(_ sender: Any) {
         valueDisplay.text=(valueDisplay.text ?? "0") + "+"
     }
     
     @IBAction func remainder(_ sender: Any) {
-        valueDisplay.text = (valueDisplay.text ?? "0")
+        valueDisplay.text = (valueDisplay.text ?? "0") + "%"
     }
     
     @IBAction func mulMinusOne(_ sender: Any) {
@@ -82,4 +151,6 @@ class ViewController: UIViewController {
         valueDisplay.text="-" + (valueDisplay.text ?? "0")
     }
 }
+
+
 
